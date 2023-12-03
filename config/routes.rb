0c4bb29e416b0bf09385761fe56096cb2522b8ff
Dylan Perry <dylanbperry@gmail.com>
2023-12-02 16:31:10 -0800

    Add dev routes: show, index_dev_games. Add games routes: show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,15 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index"
 
   get "/devs", to: "devs#index"
+  get "/devs/:id", to: "devs#show"
+  get "/devs/:id/games", to: "devs#index_dev_games"
   get "/devs/new", to: "devs#new"
+  post "/devs", to: "devs#create"
 
   get "/games", to: "games#index"
+  get "/games/:id", to: "games#show"
   get "/games/new", to: "games#new"
+  post "/games", to: "games#create"
+  
+
 end
