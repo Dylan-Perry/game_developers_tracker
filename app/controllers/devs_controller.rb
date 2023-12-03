bb9a0d91698006ab213632e0b5878027f0d1c7cf
Dylan Perry <dylanbperry@gmail.com>
2023-12-02 16:25:22 -0800

    Add methods: show, index_dev_games

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -10,4 +10,12 @@ class DevsController < ApplicationController
     def create
         
     end
+    
+    def show
+        @dev = Dev.find(params[:id])
+    end
+
+    def index_dev_games
+        self.show
+    end
 end
