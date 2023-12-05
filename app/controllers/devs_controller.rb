244a014a10be868cd17543f4b5559ff4f7289c94
Dylan Perry <dylanbperry@gmail.com>
2023-12-04 16:22:24 -0800

    Add methods: create, sort (self)

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -1,6 +1,10 @@
 class DevsController < ApplicationController
     def index
-        @devs = Dev.order(created_at: :DESC)
+        if params[:sort] # Useful later for sorting, US-16; check custom sort method below
+            @devs = Dev.sort(params[:sort])
+        else
+            @devs = Dev.order(created_at: :DESC)
+        end
     end
 
     def new
@@ -8,14 +12,24 @@ class DevsController < ApplicationController
     end
 
     def create
-        
+        dev = Dev.new({
+            name: params[:name],
+            employees: params[:employees],
+            operational: params[:operational]
+        })
+
+        dev.save
+
+        redirect_to "/devs"
     end
     
     def show
         @dev = Dev.find(params[:id])
     end
 
-    def index_dev_games
-        self.show
+    def self.sort(type) # see notes above on sorting
+        if type == "alpha"
+            Dev.order(created_at: :DESC)
+        end
     end
 end
